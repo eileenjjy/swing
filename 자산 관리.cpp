160f6b9a4a01596614b75e1a457e740214ca2c2b
eileenjjy <102503055+eileenjjy@users.noreply.github.com>
2022-05-10 23:36:01 +0900

    Add files via upload

---
+++ 자산 관리.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+void main()
+{
+
+	char bank_name[4] = { '하나', '우리', '신한', '국민' };
+	char* bn = bank_name;
+		* bn = 5000;
+		*(bn + 1) = 4000;
+		*(bn + 2) = 0;
+		*(bn + 3) = 10;
+
+	cout << "*********** swing 자산 관리 서비스 ***************";
+	
+
+	
+}
+
+
+void myaccounts()
+{
+
+
+}
+
+
+void total()
+{
+
+
+
+}
